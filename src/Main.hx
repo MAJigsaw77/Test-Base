3b81db9f4fb0a31aea562df6a3b0a8a658f84f88
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-05 08:37:09 +0200

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -15,7 +15,7 @@ class Main extends Sprite
 
 		final zoom:Float = Math.min(Lib.current.stage.stageWidth / Lib.application.window.width, Lib.current.stage.stageHeight / Lib.application.window.height);
 
-		addChild(new FlxGame(Math.ceil(Lib.current.stage.stageWidth / zoom), Math.ceil(Lib.current.stage.stageHeight / zoom), MainState, zoom, Lib.current.stage.frameRate, Lib.current.stage.frameRate, false, false));
+		addChild(new FlxGame(Math.ceil(Lib.current.stage.stageWidth / zoom), Math.ceil(Lib.current.stage.stageHeight / zoom), MainState, zoom, Std.int(Lib.current.stage.frameRate), Std.int(Lib.current.stage.frameRate), false, false));
 		addChild(new Overlay(10, 10, 0xFFFFFF));
 	}
 }
