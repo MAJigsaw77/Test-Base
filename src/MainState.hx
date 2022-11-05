7069b27bea8bdfaf36dbbd1eda566ab5b12a5c0b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-05 07:49:11 +0200

    Update MainState.hx

--- src/MainState.hx
+++ src/MainState.hx
@@ -1,5 +1,8 @@
 package;
 
+#if android
+import android.widget.Toast;
+#end
 import flixel.FlxState;
 
 class MainState extends FlxState
@@ -7,6 +10,10 @@ class MainState extends FlxState
 	override function create()
 	{
 		super.create();
+
+		#if android
+		Toast.makeText('HELLO WORLD!!!!', Toast.LENGTH_LONG);
+		#end
 	}
 
 	override function update(elapsed:Float)
