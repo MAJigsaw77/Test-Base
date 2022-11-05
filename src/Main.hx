eb07a953ea04a3e2ac307016690d445ba405c0a8
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-05 07:13:36 +0200

    Nice

--- src/Main.hx
+++ src/Main.hx
@@ -7,31 +7,17 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
-	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-
 	public function new()
 	{
 		super();
 
 		SUtil.uncaughtErrorHandler();
 
-		final stageWidth:Int = Lib.current.stage.stageWidth;
-		final stageHeight:Int = Lib.current.stage.stageHeight;
-
-		if (zoom == -1)
-		{
-			var ratioX:Float = stageWidth / gameWidth;
-			var ratioY:Float = stageHeight / gameHeight;
-			zoom = Math.min(ratioX, ratioY);
-			gameWidth = Math.ceil(stageWidth / zoom);
-			gameHeight = Math.ceil(stageHeight / zoom);
-		}
-
 		SUtil.checkPermissions();
 
-		addChild(new FlxGame(gameWidth, gameHeight, MainState, zoom, 60, 60, false, false));
+		var zoom:Float = Math.min(Lib.current.stage.stageWidth / Lib.application.window.width, Lib.current.stage.stageHeight / Lib.application.window.height);
+		addChild(new FlxGame(Math.ceil(Lib.current.stage.stageWidth / zoom), Math.ceil(Lib.current.stage.stageHeight / zoom), MainState, zoom, 60, 60, false, false));
+
 		addChild(new FPS(10, 10, 0xFFFFFF));
 	}
 }
