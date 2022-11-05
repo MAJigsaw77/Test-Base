47769a92fe63feec25349b689f55f9dae7dc6b50
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-05 07:27:46 +0200

    Create Overlay.hx

---
+++ src/Overlay.hx
@@ -0,0 +1,67 @@
+package;
+
+import haxe.Timer;
+import openfl.events.Event;
+import openfl.system.System;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+
+class Overlay extends TextField
+{
+	private var times:Array<Float> = [];
+	private var memPeak:Float = 0;
+
+	public function new(x:Float, y:Float, color:Int)
+	{
+		super();
+
+		this.x = x;
+		this.y = x;
+		this.autoSize = LEFT;
+		this.selectable = false;
+		this.mouseEnabled = false;
+		this.defaultTextFormat = new TextFormat('_sans', 14, 0xFFFFFF);
+
+		addEventListener(Event.ENTER_FRAME, function(e:Event)
+		{
+			var now = Timer.stamp();
+			times.push(now);
+			while (times[0] < now - 1)
+				times.shift();
+
+			var currentFrames:Int = times.length;
+			if (currentFrames > PreferencesData.framerate)
+				currentFrames = PreferencesData.framerate;
+
+			if (currentFrames <= PreferencesData.framerate / 4)
+				textColor = 0xFFFF0000;
+			else if (currentFrames <= PreferencesData.framerate / 2)
+				textColor = 0xFFFFFF00;
+			else
+				textColor = 0xFFFFFFFF;
+
+			var mem:Float = System.totalMemory;
+
+			if (mem > memPeak)
+				memPeak = mem;
+
+			if (visible)
+				text = currentFrames + ' FPS\n' + getInterval(mem) + ' / ' + getInterval(memPeak) + '\n';
+		});
+	}
+
+	private function getInterval(size:Float):String
+	{
+		var data:Int = 0;
+
+		final intervalArray:Array<String> = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+		while (size > 1024 && data < intervalArray.length - 1)
+		{
+			data++;
+			size = size / 1024;
+		}
+
+		size = Math.round(size * 100) / 100;
+		return size + ' ' + intervalArray[data];
+	}
+}
