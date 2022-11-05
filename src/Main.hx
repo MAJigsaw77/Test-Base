41e6a29102bd2ca1b5d3119063ff6a200889cae7
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-05 07:28:11 +0200

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -2,7 +2,6 @@ package;
 
 import flixel.FlxGame;
 import openfl.Lib;
-import openfl.display.FPS;
 import openfl.display.Sprite;
 
 class Main extends Sprite
@@ -17,6 +16,6 @@ class Main extends Sprite
 		final zoom:Float = Math.min(Lib.current.stage.stageWidth / Lib.application.window.width, Lib.current.stage.stageHeight / Lib.application.window.height);
 
 		addChild(new FlxGame(Math.ceil(Lib.current.stage.stageWidth / zoom), Math.ceil(Lib.current.stage.stageHeight / zoom), MainState, zoom, 60, 60, false, false));
-		addChild(new FPS(10, 10, 0xFFFFFF));
+		addChild(new Overlay(10, 10, 0xFFFFFF));
 	}
 }
