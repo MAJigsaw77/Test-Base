624fbefcead40ebf7aaab99b152666a1394d8b8c
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-05 17:39:31 +0200

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -13,9 +13,9 @@ class Main extends Sprite
 		SUtil.uncaughtErrorHandler();
 		SUtil.checkPermissions();
 
+		final zoom:Float = Math.min(Lib.current.stage.stageWidth / Lib.application.window.width, Lib.current.stage.stageHeight / Lib.application.window.height);
 		final width:Int = Math.ceil(Lib.current.stage.stageWidth / zoom);
 		final height:Int = Math.ceil(Lib.current.stage.stageHeight / zoom);
-		final zoom:Float = Math.min(Lib.current.stage.stageWidth / Lib.application.window.width, Lib.current.stage.stageHeight / Lib.application.window.height);
 		final framerate:Int = Std.int(Lib.current.stage.frameRate);
 
 		addChild(new FlxGame(width, height, MainState, zoom, framerate, framerate, false, false));
