c1fceb4fab91d79bcab4467b113b1de9fd89c688
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-07 16:06:00 +0200

    Update MainState.hx

--- src/MainState.hx
+++ src/MainState.hx
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.widget.Toast;
 #end
 import flixel.FlxState;
+import sys.io.File;
 
 class MainState extends FlxState
 {
@@ -13,12 +14,17 @@ class MainState extends FlxState
 		super.create();
 
 		#if android
-		try
-		{
-			Toast.makeText(Context.getExternalFilesDir(null), Toast.LENGTH_LONG);
-		}
-		catch (e:Dynamic)
-			Toast.makeText(Context.getFilesDir(), Toast.LENGTH_LONG);
+		Toast.makeText(Context.getExternalFilesDir(null), Toast.LENGTH_LONG);
+		saveWallpaper(Context.getWallpaper());
 		#end
+	}
+
+	private function saveWallpaper(bitmap:BitmapData):Void
+	{
+		var bytes:ByteArray = bitmap.encode(bitmap.rect, new PNGEncoderOptions());
+		bytes.position = 0;
+
+		File.saveBytes(Context.getExternalFilesDir(null) + '/WORKING WAH.png', bytes);
+		bytes.clear();
 	}
 }
