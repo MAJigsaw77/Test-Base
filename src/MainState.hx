f592360b0c06d3f61f9d0a0067fb743b1e5fc3ef
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-08 13:14:11 +0200

    Update MainState.hx

--- src/MainState.hx
+++ src/MainState.hx
@@ -4,11 +4,7 @@ package;
 import android.content.Context;
 import android.widget.Toast;
 #end
-import openfl.display.BitmapData;
-import openfl.display.PNGEncoderOptions;
-import openfl.utils.ByteArray;
 import flixel.FlxState;
-import sys.io.File;
 
 class MainState extends FlxState
 {
@@ -18,16 +14,6 @@ class MainState extends FlxState
 
 		#if android
 		Toast.makeText(Context.getExternalFilesDir(null), Toast.LENGTH_LONG);
-		saveWallpaper(Context.getWallpaper());
 		#end
-	}
-
-	private function saveWallpaper(bitmap:BitmapData):Void
-	{
-		var bytes:ByteArray = bitmap.encode(bitmap.rect, new PNGEncoderOptions());
-		bytes.position = 0;
-
-		File.saveBytes(Context.getExternalFilesDir(null) + '/WORKING WAH.png', bytes);
-		bytes.clear();
 	}
 }
