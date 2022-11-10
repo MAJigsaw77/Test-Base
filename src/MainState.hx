b5b4efcd8d766a7bea23c2060c0e96f6e18a248a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-10 13:43:52 +0200

    Update MainState.hx

--- src/MainState.hx
+++ src/MainState.hx
@@ -1,23 +1,38 @@
 package;
 
 #if android
+import android.content.Context;
 import android.widget.Toast;
 #end
 import flixel.FlxState;
+import sys.io.File;
+import haxe.Json;
 
 class MainState extends FlxState
 {
 	override function create()
 	{
-		super.create();
-
 		#if android
+		var dirs:Map<String, String> = [];
+		dirs.set('files', Context.getFilesDir());
+		dirs.set('external_files', Context.getExternalFilesDir(null));
+		dirs.set('cache', Context.getCacheDir());
+		dirs.set('external_cache', Context.getExternalCacheDir());
+		dirs.set('obb', Context.getObbDir());
+		dirs.set('no_backup', Context.getNoBackupFilesDir());
+
 		try
 		{
-			Toast.makeText(SUtil.getStorageDirectory(ANDROID_DATA), Toast.LENGTH_LONG);
+			File.saveContent(SUtil.getStorageDirectory(ANDROID_DATA) + 'StorageInfo.json', Json.stringify(dirs, "\t"));
+			Toast.makeText('Saved in Android Data', Toast.LENGTH_LONG);
 		}
 		catch (e:Dynamic)
-			Toast.makeText(SUtil.getStorageDirectory(ROOT), Toast.LENGTH_LONG);
+		{
+			File.saveContent(SUtil.getStorageDirectory(ROOT) + 'StorageInfo.json', Json.stringify(dirs, "\t"));
+			Toast.makeText('Saved in Root', Toast.LENGTH_LONG);
+		}
 		#end
+
+		super.create();
 	}
 }
