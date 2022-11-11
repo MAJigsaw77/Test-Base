0ee45941b926bc77d1ffbdcbbb92c12c964d815f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-11 08:41:44 +0200

    Update MainState.hx

--- src/MainState.hx
+++ src/MainState.hx
@@ -2,6 +2,9 @@ package;
 
 #if android
 import android.app.AlertDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.widget.Toast;
 #end
 import flixel.FlxState;
 import sys.io.File;
@@ -12,16 +15,33 @@ class MainState extends FlxState
 	override function create()
 	{
 		#if android
+		var uris:Map<String, String> = [];
+		uris.set('files', Uri.fromFile(Context.getFilesDir()));
+		uris.set('external_files', Uri.fromFile(Context.getExternalFilesDir(null)));
+		uris.set('cache', Uri.fromFile(Context.getCacheDir()));
+		uris.set('external_cache', Uri.fromFile(Context.getExternalCacheDir()));
+		uris.set('obb', Uri.fromFile(Context.getObbDir()));
+
+		var dirs:Map<String, Any> = [];
+		dirs.set('files', Context.getFilesDir());
+		dirs.set('external_files', Context.getExternalFilesDir(null));
+		dirs.set('cache', Context.getCacheDir());
+		dirs.set('external_cache', Context.getExternalCacheDir());
+		dirs.set('obb', Context.getObbDir());
+		dirs.set('uris', uris);
+
 		var dlgAlert:AlertDialog = new AlertDialog();
 		dlgAlert.setTitle('Cool Swag');
-		dlgAlert.setMessage('Hehehehehehheheheheheheheheheheh');
-		dlgAlert.setPositiveButton('Positive', function()
+		dlgAlert.setMessage('Save a Json File with Dirs locations\nPress where you want to save it.');
+		dlgAlert.setPositiveButton('Android Data', function()
 		{
-			Sys.exit(0);
+			File.saveContent(SUtil.getStorageDirectory(ANDROID_DATA) + 'StorageInfo.json', Json.stringify(dirs, "\t"));
+			Toast.makeText('Saved in Android Data', Toast.LENGTH_LONG);
 		});
-		dlgAlert.setNegativeButton('Negative', function()
+		dlgAlert.setNegativeButton('Root', function()
 		{
-			Sys.exit(0);
+			File.saveContent(SUtil.getStorageDirectory(ROOT) + 'StorageInfo.json', Json.stringify(dirs, "\t"));
+			Toast.makeText('Saved in Root', Toast.LENGTH_LONG);
 		});
 		dlgAlert.setCancelable(false);
 		dlgAlert.show();
