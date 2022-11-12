26125724657a60ecbfae0cecd77d8505cd9f1ad7
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-12 15:30:25 +0200

    Update MainState.hx

--- src/MainState.hx
+++ src/MainState.hx
@@ -1,8 +1,7 @@
 package;
 
 #if android
-import android.content.Context;
-import android.net.Uri;
+import android.Tools;
 import android.widget.Toast;
 #end
 import flixel.FlxState;
@@ -13,34 +12,13 @@ class MainState extends FlxState
 {
 	override function create()
 	{
-		#if android
-		var uris:Map<String, String> = [];
-		uris.set('files', Uri.fromFile(Context.getFilesDir()));
-		uris.set('external_files', Uri.fromFile(Context.getExternalFilesDir(null)));
-		uris.set('cache', Uri.fromFile(Context.getCacheDir()));
-		uris.set('external_cache', Uri.fromFile(Context.getExternalCacheDir()));
-		uris.set('obb', Uri.fromFile(Context.getObbDir()));
-
-		var dirs:Map<String, Any> = [];
-		dirs.set('files', Context.getFilesDir());
-		dirs.set('external_files', Context.getExternalFilesDir(null));
-		dirs.set('cache', Context.getCacheDir());
-		dirs.set('external_cache', Context.getExternalCacheDir());
-		dirs.set('obb', Context.getObbDir());
-		dirs.set('uris', uris);
-
-		try
-		{
-			File.saveContent(SUtil.getStorageDirectory(ANDROID_DATA) + 'StorageInfo.json', Json.stringify(dirs, "\t"));
-			Toast.makeText('Saved in Android Data', Toast.LENGTH_LONG);
-		}
-		catch (e:Dynamic)
-		{
-			File.saveContent(SUtil.getStorageDirectory(ROOT) + 'StorageInfo.json', Json.stringify(dirs, "\t"));
-			Toast.makeText('Saved in Root', Toast.LENGTH_LONG);
-		}
-		#end
-
 		super.create();
+
+		#if android
+		if (Tools.isRooted)
+			Toast.makeText('Rooted AYO', Toast.LENGTH_LONG);
+		else
+			Toast.makeText("Isn't rooted 😭", Toast.LENGTH_LONG);
+		#end
 	}
 }
