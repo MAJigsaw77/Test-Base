df4209548d1f5f88c220744b90fe0ac354126492
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-13 11:30:19 +0200

    Update MainState.hx

--- src/MainState.hx
+++ src/MainState.hx
@@ -2,9 +2,12 @@ package;
 
 #if android
 import android.Tools;
+import android.net.Uri;
 import android.widget.Toast;
+import videoview.VideoView;
 #end
 import flixel.FlxState;
+import sys.FileSystem;
 
 class MainState extends FlxState
 {
@@ -17,6 +20,9 @@ class MainState extends FlxState
 			Toast.makeText('Rooted AYO', Toast.LENGTH_LONG);
 		else
 			Toast.makeText("Isn't rooted 😭", Toast.LENGTH_LONG);
+
+		if (FileSystem.exists(SUtil.getStorageDirectory() + 'video.mp4'))
+			VideoView.playVideo(Uri.fromFile(SUtil.getStorageDirectory() + 'video.mp4'));
 		#end
 	}
 }
