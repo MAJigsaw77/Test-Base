1e857756dd654449866344db68d272aa8a5fff6d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-26 15:10:33 +0200

    Test

--- src/State.hx
+++ src/State.hx
@@ -1,9 +1,15 @@
 package;
 
 #if android
+import android.FileBrowser;
+import android.callback.CallBack;
+import android.callback.CallBackEvent;
+import android.os.Environment;
+import android.net.Uri;
 import android.widget.Toast;
 #end
 import ffmpeg.Version;
+import ffmpeg.openfl.OpenFLBitmapVideo;
 import flixel.FlxG;
 import flixel.FlxState;
 
@@ -17,13 +23,49 @@ class State extends FlxState
 		FlxG.android.preventDefaultKeys = [BACK];
 		#end
 
-		super.create();
-
 		Toast.makeText('libavcodec version: ' + Version.getLibavcodecVersion() + '\n'
 			 + 'libavutil version: ' + Version.getLibavutilVersion() + '\n'
 			 + 'libavformat version: ' + Version.getLibavformatVersion() + '\n'
 			 + 'libavfilter version: ' + Version.getLibavfilterVersion() + '\n'
 			 + 'libswresample version: ' + Version.getLibswresampleVersion() + '\n'
-			 + 'libswscale version: ' + Version.getLibswscaleVersion() + '\n', Toast.LENGTH_LONG);
+			 + 'libswscale version: ' + Version.getLibswscaleVersion(), Toast.LENGTH_LONG);
+
+		super.create();
+
+		#if android
+		CallBack.init();
+		CallBack.addEventListener(CallBackEvent.ACTIVITY_RESULT, onActivityResult);
+		FileBrowser.open(FileBrowser.GET_CONTENT);
+		#end
+	}
+
+	private function onActivityResult(e:CallBackEvent)
+	{
+		if (e.content != null && e.content.data != null)
+		{
+			var daPath:String = e.content.data.getPath;
+
+			if (daPath.contains('/document/primary:'))
+				daPath = daPath.replace('/document/primary:', Environment.getExternalStorageDirectory() + '/');
+			else if (daPath.contains('/document/'))
+			{
+				final daOldStorageEnter:String = daPath.substring(0, daPath.indexOf(':') + 1);
+				daPath = daPath.replace(daOldStorageEnter, daOldStorageEnter.replace('/document/', '/storage/').replace(':', '/'));
+			}
+
+			if (FileSystem.exists(daPath))
+			{
+				var video:OpenFLBitmapVideo = new OpenFLBitmapVideo();
+				video.open(daPath);
+				FlxG.addChildBelowMouse(video);
+				video.play();
+			}
+			else
+			{
+				Toast.makeText(daPath + ": Doesn't exists", Toast.LENGTH_LONG);
+				CallBack.removeEventListener(CallBackEvent.ACTIVITY_RESULT, onActivityResult);
+				FlxG.resetGame();
+			}
+		}
 	}
 }
