69a9db62a95f63107ae38c6c85d22c44c45e492f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-28 00:39:49 +0200

    Update State.hx

--- src/State.hx
+++ src/State.hx
@@ -2,10 +2,14 @@ package;
 
 #if android
 import android.content.Context;
+inport android.kizzy.KizzyClient;
 import android.widget.Toast;
 #end
 import flixel.FlxG;
 import flixel.FlxState;
+import openfl.Lib;
+import haxe.Json;
+import sys.io.File;
 import sys.FileSystem;
 
 using StringTools;
@@ -25,6 +29,26 @@ class State extends FlxState
 		}
 		catch (e:Dynamic)
 			Toast.makeText('FUCK ಠ⁠︵⁠ಠ' + '\n' + e, Toast.LENGTH_LONG);
+
+		if (FileSystem.exists(SUtil.getStoragePath() + 'token.json'))
+		{
+			var token:Dynamic = Json.parse(File.getContent(SUtil.getStoragePath() + 'token.json')));
+
+			var kizzyClient:KizzyClient = new KizzyClient(token.value);
+			kizzyClient.setApplicationID('378534231036395521');
+			kizzyClient.setName('Kizzy RPC Client Android');
+			kizzyClient.setDetails('Hello World!');
+			kizzyClient.setType(3);
+			kizzyClient.setState('State');
+			kizzyClient.setStatus('online');
+			kizzyClient.rebuildClient();
+
+			Lib.application.window.onClose.add(function()
+			{
+				if (kizzyClient.isClientRunning())
+					kizzyClient.closeClient();
+			});
+		}
 
 		super.create();
 	}
