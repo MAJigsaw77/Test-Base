70b9ef7a0c042a2180d13911151c43c9c4084b7d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-29 21:34:30 +0200

    Update Overlay.hx

--- src/Overlay.hx
+++ src/Overlay.hx
@@ -7,6 +7,24 @@ import openfl.system.System;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 
+#if windows
+@:headerCode("
+#include <windows.h>
+#include <psapi.h>
+")
+#elseif mac
+@:headerCode("
+#include <unistd.h>
+#include <sys/resource.h>
+#include <mach/mach.h>
+") 
+#elseif (linux || android)
+@:headerCode("
+#include <unistd.h>
+#include <sys/resource.h>
+#include <stdio.h>
+")
+#end
 class Overlay extends TextField
 {
 	private var times:Array<Float> = [];
@@ -42,7 +60,12 @@ class Overlay extends TextField
 			else
 				textColor = 0xFFFFFFFF;
 
+			#if (desktop || android)
+			var totalMemory:Float = Std.parseFloat(getCurrentUsage());
+			#else
 			var totalMemory:Float = System.totalMemory;
+			#end
+
 			if (totalMemory > totalMemoryPeak)
 				totalMemoryPeak = totalMemory;
 
@@ -65,4 +88,44 @@ class Overlay extends TextField
 		size = Math.round(size * 100) / 100;
 		return size + ' ' + intervalArray[data];
 	}
+
+	/**
+ 	 * Returns the current resident set size (physical memory use) measured
+ 	 * in bytes, or 0 if the value cannot be determined on this OS.
+	 */
+	#if windows
+	@:functionCode("
+		PROCESS_MEMORY_COUNTERS info;
+		GetProcessMemoryInfo(GetCurrentProcess(), &info, sizeof(info));
+		return (size_t)info.WorkingSetSize;
+	")
+	#elseif mac
+	@:functionCode("
+    		struct mach_task_basic_info info;
+		mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
+		if (task_info(mach_task_self(), MACH_TASK_BASIC_INFO, (task_info_t)&info, &infoCount) != KERN_SUCCESS)
+			return (size_t)0L;
+
+		return (size_t)info.resident_size;
+	")
+	#elseif (linux || android)
+	@:functionCode('
+		long rss = 0L;
+		FILE* fp = NULL;
+
+		if ((fp = fopen("/proc/self/statm", "r")) == NULL)
+			return (size_t)0L;
+
+		if (fscanf(fp, "%*s%ld", &rss) != 1)
+		{
+			fclose(fp);
+			return (size_t)0L;
+		}
+
+		fclose(fp);
+		return (size_t)rss * (size_t)sysconf( _SC_PAGESIZE);
+	')
+	#end
+	private function getCurrentUsage():Dynamic
+		return 0;
 }
