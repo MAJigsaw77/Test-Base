9ce0c09fac41e5599ee37305843805f28db6e6d4
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-05 20:51:39 +0200

    Update State.hx

--- src/State.hx
+++ src/State.hx
@@ -48,16 +48,17 @@ class State extends FlxState
 
 			if (FileSystem.exists(daPath))
 			{
-				var video:VideoHandler = new VideoHandler();
-				video.canUseAutoResize = false;
-				video.width = 640;
-				video.height = 360;
+				var video:VideoSprite = new VideoSprite(0, 0);
+				video.bitmap.canUseAutoResize = false;
+				video.bitmap.width = 640;
+				video.bitmap.height = 360;
 				video.finishCallback = function()
 				{
 					CallBack.removeEventListener(CallBackEvent.ACTIVITY_RESULT, onActivityResult);
 					FlxG.resetGame();
 				}
 				video.playVideo(daPath);
+				add(video);
 			}
 			else
 			{
