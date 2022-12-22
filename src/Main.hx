47f2a626c1779134b95f23fae8c9e18de4fff155
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-22 12:40:58 +0200

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -1,12 +1,6 @@
 package;
 
-#if android
-import android.widget.Toast;
-#end
-import openfl.Lib;
 import openfl.display.Sprite;
-import vlc.VLCBitmap;
-import sys.FileSystem;
 
 class Main extends Sprite
 {
@@ -17,17 +11,6 @@ class Main extends Sprite
 		SUtil.uncaughtErrorHandler();
 		SUtil.checkPermissions();
 
-		var vlc:VLCBitmap = new VLCBitmap();
-		addChild(vlc);
-
 		addChild(new Overlay(10, 10));
-
-		if (FileSystem.exists(SUtil.getStorageDirectory() + 'video.mp4'))
-		{
-			vlc.play(SUtil.getStorageDirectory() + 'video.mp4');
-			#if android
-			Toast.makeText('Should start playing?', Toast.LENGTH_LONG, 17);
-			#end
-		}
 	}
 }
