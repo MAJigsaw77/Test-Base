072dccab16a70eab328dd94268d1d39eb381b318
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-23 08:36:21 +0200

    Update PlayState.hx

--- src/game/states/PlayState.hx
+++ src/game/states/PlayState.hx
@@ -1,21 +1,27 @@
 package game.states;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import ffmpeg.openfl.OpenFLBitmapVideo;
+import ffmpeg.Version;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
+		var versions:Array<String> = [];
+		versions.push('libavcodec version: ${Version.getLibavcodecVersion()}');
+		versions.push('libavutil version: ${Version.getLibavutilVersion()}');
+		versions.push('libavformat version: ${Version.getLibavformatVersion()}');
+		versions.push('libavfilter version: ${Version.getLibavfilterVersion()}');
+		versions.push('libswresample version: ${Version.getLibswresampleVersion()}');
+		versions.push('libswscale version: ${Version.getLibswscaleVersion()}');
+		Toast.makeText(versions.join('\n'), Toast.LENGTH_SHORT);
+
 		super.create();
 
-		var bg:VideoHandler = new VideoHandler();
-		bg.canSkip = false;
-		bg.canUseAutoResize = false;
-		bg.playVideo(SUtil.getStorageDirectory() + 'assets/videos/sarv.mp4', true);
-	}
-
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
+		var video:OpenFLBitmapVideo = new OpenFLBitmapVideo();
+		FlxG.addChildBelowMouse(video);
+		video.open(SUtil.getStorageDirectory() + 'assets/videos/sarv.gif');
 	}
 }
