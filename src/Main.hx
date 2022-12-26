d7a69e77a09662bd7da2a4694447e8fcd4f33d0a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-26 22:28:08 +0200

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -9,13 +9,6 @@ import game.states.PlayState;
 
 class Main extends Sprite
 {
-	var game:Dynamic = {
-		width: 1280,
-		height: 720,
-		main_state: PlayState,
-		overlay_size: 15
-	};
-
 	public function new()
 	{
 		super();
@@ -24,7 +17,8 @@ class Main extends Sprite
 		SUtil.checkPermissions();
 
 		final framerate:Int = Std.int(Lib.current.stage.frameRate);
-		addChild(new FlxGame(game.width, game.height, game.main_state, framerate, framerate));
-		addChild(new Overlay(10, 10)); // it's annoying to have it so small as to be unable to read it.
+		addChild(new FlxGame(1280, 720, PlayState, framerate, framerate));
+
+		addChild(new Overlay(10, 10, 1280, 720));
 	}
 }
