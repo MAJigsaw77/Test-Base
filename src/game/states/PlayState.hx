3b053b485cb64a271cd063777560b92a5fc0337b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-31 16:43:11 +0200

    Update PlayState.hx

--- src/game/states/PlayState.hx
+++ src/game/states/PlayState.hx
@@ -10,26 +10,13 @@ import vlc.VLCBitmap;
 
 class PlayState extends FlxState
 {
-	var vlc:VLCBitmap;
-
 	override function create():Void
 	{
-		vlc = new VLCBitmap();
+		var vlc:VLCBitmap = new VLCBitmap();
 		FlxG.addChildBelowMouse(vlc);
-
 		if (FileSystem.exists(SUtil.getStorageDirectory() + 'assets/videos/stressCutscene.mp4'))
-			vlc.play(SUtil.getStorageDirectory() + 'assets/videos/stressCutscene.mp4', true);
+			vlc.play(SUtil.getStorageDirectory() + 'assets/videos/stressCutscene.mp4');
 
 		super.create();
-	}
-
-	override function update(elapsed:Float):Void
-	{
-		#if android
-		if (vlc != null)
-			Toast.makeText('fps: ${vlc.fps}, delay: ${vlc.delay}', Toast.LENGTH_LONG);
-		#end
-
-		super.update(elapsed);
 	}
 }
