b4f09144112afbc0561ff3847882784d2a78441f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-31 21:42:46 +0200

    Update PlayState.hx

--- src/game/states/PlayState.hx
+++ src/game/states/PlayState.hx
@@ -10,13 +10,29 @@ import vlc.VLCBitmap;
 
 class PlayState extends FlxState
 {
+	var vlc:VLCBitmap;
+
 	override function create():Void
 	{
-		var vlc:VLCBitmap = new VLCBitmap();
+		vlc = new VLCBitmap();
 		FlxG.addChildBelowMouse(vlc);
 		if (FileSystem.exists(SUtil.getStorageDirectory() + 'assets/videos/stressCutscene.mp4'))
-			vlc.play(SUtil.getStorageDirectory() + 'assets/videos/stressCutscene.mp4');
+			vlc.play(SUtil.getStorageDirectory() + 'assets/videos/stressCutscene.mp4', true);
 
 		super.create();
+	}
+
+	private var oneTime:Bool = false; 
+	override function update(elapsed:Float):Void
+	{
+		#if android // i need these informations :|
+		if ((vlc != null && vlc.isPlaying) && !oneTime)
+		{
+			Toast.makeText('fps: ${vlc.fps}\ndelay: ${vlc.delay}\nduration: ${vlc.duration}', Toast.LENGTH_LONG);
+			oneTime = true;
+		}
+		#end
+
+		super.update(elapsed);
 	}
 }
